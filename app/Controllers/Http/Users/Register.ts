8e55651e81f913355739cbc701cd6e274da918e7
Eric Almeida <ericalmeida.sp@gmail.com>
2022-03-29 23:22:10 -0300

    email

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -2,6 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { User } from 'App/Models'
 import { RegisterValidator } from 'App/Validators/User/Register'
 import faker from 'faker'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class UserRegisterController {
   public async store({ request }: HttpContextContract) {
@@ -14,9 +15,18 @@ export default class UserRegisterController {
     user.related('keys').create({ key })
 
     const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+    // envio
+
+    await Mail.send((message) => {
+      message.to(email)
+      message.from('contato@fb.com', 'FacebookTest')
+      message.subject('New Account')
+      message.htmlView('emails/register', { link })
+    })
   }
 
-  public async show({ }: HttpContextContract) { }
+  public async show({}: HttpContextContract) {}
 
-  public async update({ }: HttpContextContract) { }
+  public async update({}: HttpContextContract) {}
 }
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
 import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
-import { UserKey } from 'app/Models'
+import { UserKey } from 'App/Models'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
