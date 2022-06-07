b2e773de79fdcf29fc7b3228122dc670485f6763
Éric Almeida <ericalmeida.sp@gmail.com>
2022-06-07 18:51:27 -0300

    timeline com users posts

--- app/Controllers/Http/Posts/Main.ts
+++ app/Controllers/Http/Posts/Main.ts
@@ -11,9 +11,69 @@ export default class PostsController {
 
     const user = username ? await User.findByOrFail('username', username) : auth.user!
 
-    // se nao tem username, carrega a timeline principal, com os posts de quem o user segue
+    //pega os posts do user.auth ou username
+
+    await user.load('posts', (query) => {
+      query.orderBy('id', 'desc')
+
+      query.preload('comments', (query) => {
+        query.preload('user', (query) => {
+          query.select(['id', 'name', 'username'])
+          query.preload('avatar')
+        })
+      })
+
+      query.preload('reactions', () => {
+        query.where('userId', auth.user!.id).first()
+      })
+
+      query.withCount('comments')
+
+      query.preload('media')
+
+      query.preload('user', (query) => {
+        query.select(['id', 'name', 'username'])
+        query.preload('avatar')
+      })
+
+      // counets reactions
+      // likeCount
+      query.withCount('reactions', (query) => {
+        query.where('type', 'like')
+        query.as('likeCount')
+      })
+
+      // hahaCount
+      query.withCount('reactions', (query) => {
+        query.where('type', 'haha')
+        query.as('hahaCount')
+      })
+
+      // sadCount
+      query.withCount('reactions', (query) => {
+        query.where('type', 'sad')
+        query.as('sadCount')
+      })
+
+      // loveCount
+      query.withCount('reactions', (query) => {
+        query.where('type', 'love')
+        query.as('loveCount')
+      })
+
+      // angryCount
+      query.withCount('reactions', (query) => {
+        query.where('type', 'angry')
+        query.as('angryCount')
+      })
+    })
+
+   
+
+    // se nao tem username, carrega a timeline principal, com os posts de quem o user segue junto com os dele
+    //se tem username, pula esse tela e entrega direto os posts do username solicitado via query
     if (!username) {
-      const user = auth.user!
+      // const user = auth.user!
       // carrega os posts com os users
       await user.load('following', (query) => {
         query.preload('posts', (query) => {
@@ -83,71 +143,20 @@ export default class PostsController {
 
       const getUsersPosts = allposts(user.following.map((user) => user.posts))
 
+      // junta os posts do criador com o dos seguidores
+
+      const joinUsersPostsWithFollowings = [...getUsersPosts, ...user.posts]
+
       // ordena os posts
-      const orderPostsByRecently = getUsersPosts.sort((a, b) => {
+      const orderPostsByRecently = joinUsersPostsWithFollowings.sort((a, b) => {
         return b.id - a.id
       })
 
       return orderPostsByRecently
-    }
-
-    // se tem username, pega os posts do username
-    await user.load('posts', (query) => {
-      query.orderBy('id', 'desc')
-
-      query.preload('comments', (query) => {
-        query.preload('user', (query) => {
-          query.select(['id', 'name', 'username'])
-          query.preload('avatar')
-        })
-      })
-
-      query.preload('reactions', () => {
-        query.where('userId', auth.user!.id).first()
-      })
-
-      query.withCount('comments')
-
-      query.preload('media')
-
-      query.preload('user', (query) => {
-        query.select(['id', 'name', 'username'])
-        query.preload('avatar')
-      })
-
-      // counets reactions
-      // likeCount
-      query.withCount('reactions', (query) => {
-        query.where('type', 'like')
-        query.as('likeCount')
-      })
-
-      // hahaCount
-      query.withCount('reactions', (query) => {
-        query.where('type', 'haha')
-        query.as('hahaCount')
-      })
-
-      // sadCount
-      query.withCount('reactions', (query) => {
-        query.where('type', 'sad')
-        query.as('sadCount')
-      })
-
-      // loveCount
-      query.withCount('reactions', (query) => {
-        query.where('type', 'love')
-        query.as('loveCount')
-      })
-
-      // angryCount
-      query.withCount('reactions', (query) => {
-        query.where('type', 'angry')
-        query.as('angryCount')
-      })
-    })
-
+    } 
+    
     return user.posts
+        
   }
 
   public async show({ params }: HttpContextContract) {
@@ -195,7 +204,15 @@ export default class PostsController {
   public async store({ request, auth }: HttpContextContract) {
     const data = await request.validate(StoreValidator)
     const post = await auth.user!.related('posts').create(data)
-
+    await post.load('user', (query) => query.preload('avatar'))
+    await post.load('comments', (query) => {
+      query.preload('user', (query) => {
+        query.select(['id', 'name', 'username'])
+        query.preload('avatar')
+      })
+    })
+    
+    
     return post
   }
 
